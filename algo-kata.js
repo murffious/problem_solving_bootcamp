93f3d5e982ab1ed7d9e6eb34a834df391e665864
Paul Murff <titan.murff@gmail.com>
2019-09-09 22:11:11 -0600

    Create algo-kata.js

---
+++ algo-kata.js
@@ -0,0 +1,15 @@
+GOALS: time and space(memory) complexity lowest possible while considering readability/code quality/clean for maintanence and technical debt 
+
+1. compare two items/variables usually not always in a loop
+if(aVar === bVar){
+
+}
+2. divide and conquer 
+pick an index and then check that half and keep splitting until found 
+
+
+Doing coding problems is just following certain tactics kinda like karate or kata - - muscle memory kicks in and then you develop "intuition"
+
+Seeing how others solved something and learning patterns even math patterns of ways of solving stuff - 
+
+dynamic programming
