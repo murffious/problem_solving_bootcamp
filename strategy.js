a0748aeb23e5f77dbf7c8a18024030c2426f7201
Paul Murff <titan.murff@gmail.com>
2019-09-23 21:10:46 -0600

    Create strategy.js

---
+++ strategy.js
@@ -0,0 +1,7 @@
+Iteration
+while 
+for 
+nested loops 
+The process can be described using loops. An outer loop keeps track of the next flower to consider. An inner loop duplicates the fragrances, adding the current flower to the duplicates. 
+
+Ferreira Filho, Wladston. Computer Science Distilled: Learn the Art of Solving Computational Problems (p. 37). Code Energy LLC. Kindle Edition. 
